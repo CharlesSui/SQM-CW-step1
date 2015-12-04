950eb9eecc50511736ed49c946ae5f2b7307b6f2
Charles <smz5519227@gmail.com>
2015-12-04 02:09:16 +0000

    add new project

---
+++ src/g53sqm/chat/server/ServerTest.java
@@ -0,0 +1,19 @@
+package g53sqm.chat.server;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ServerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	public void checkServerMain(){
+		String[] args = new String[2];
+		Runner.main(args);
+	}
+
+}
